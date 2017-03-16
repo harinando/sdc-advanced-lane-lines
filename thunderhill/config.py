4b344a459126419e0142860345acfc98cb6d6c58
Harinando Andrianarimanana <toni.andrianaht@gmail.com>
2017-03-16 12:38:18 -0700

    Driving at 30mph on the thunderhill track

---
+++ thunderhill/config.py
@@ -0,0 +1,16 @@
+BATCH_SIZE = 128
+EPOCHS = 30
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+WIDTH = 128
+HEIGHT = 64
+# WIDTH = 320
+# HEIGHT = 160
+DEPTH = 3
+ADJUSTMENT = 0.005
+ALPHA = 0.001
+DROPOUT = 0.5
+OUTPUT = '.hdf5_checkpoints'
+COLUMNS = ["center","left","right","steering","throttle","brake","speed","position","rotation"]
+SKIP    = ["dataset_sim_000_km_few_laps"]
+SCALER = 'scaler.p'
+COLUMNS_TO_NORMALIZE = ['positionX', 'positionY', 'positionZ', 'rotationX', 'rotationY', 'rotationZ']
